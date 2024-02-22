d4d53b95196bb77bc4e3f3ba7cc7db9be3f58b37
Hammad Khan <154624434+hammadspace1@users.noreply.github.com>
2024-02-22 07:45:23 +0000

    conversion done

---
+++ basics/conversion.js
@@ -0,0 +1,15 @@
+let number = "1122"
+
+console.log(typeof number )
+
+let Nnumber = Number(number)
+
+console.log(typeof Nnumber)
+
+let isLoggedIn = true;
+
+console.log(typeof isLoggedIn)
+
+let numberboolean = Number(isLoggedIn)
+
+console.log(numberboolean)
