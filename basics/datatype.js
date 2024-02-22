ed03a961d4e866fe6ceaeff9ce7a0c407dfc3138
Hammad Khan <154624434+hammadspace1@users.noreply.github.com>
2024-02-22 09:29:12 +0000

    stack and Heap done

---
+++ basics/datatype.js
@@ -0,0 +1,9 @@
+const id = Symbol('1234')
+
+console.log(id)
+
+const anotherid = Symbol('1234')
+
+console.log(anotherid)
+
+console.log(id === anotherid)
---
+++ basics/stackandHeap.js
@@ -0,0 +1,21 @@
+let myName = "Hammad"
+
+let mysecondName = myName
+
+mysecondName = "Khan"
+
+// console.log("myName : "+myName)
+// console.log("mysecondName : "+mysecondName)
+
+let user1 = {
+    Name: "Hammad",
+    Email: "Hammad@test.com"
+}
+
+console.log(user1.Email)
+
+let user2 = user1
+
+user1.Email = "Khan@test.com"
+
+console.log(user1.Email)
