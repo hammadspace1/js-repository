dad38022e73e290abf764a87380bce861add81ce
Hammad Khan <154624434+hammadspace1@users.noreply.github.com>
2024-02-22 17:46:41 +0000

    strings done

---
+++ basics/06strings.js
@@ -0,0 +1,27 @@
+let firstName = "Hammad"
+let secondName = "Khan"
+
+// console.log(firstName+secondName)
+
+// console.log(`My full Name is ${firstName } ${secondName}`)
+
+const fullName = new String('Hammad-Khan')
+
+// console.log(fullName)
+
+const name = new String(fullName.slice(0,4)) 
+
+// console.log(name[2])
+
+// console.log(name.length)
+
+// console.log(name.toUpperCase())
+
+// console.log(name.charAt('2'))
+
+// console.log(name.indexOf('m'))
+
+console.log(name.anchor())
+
+
+
