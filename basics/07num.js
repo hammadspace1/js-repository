40aa46099f3929d4f9b5b23bf77ff85bdcd069c3
Hammad Khan <154624434+hammadspace1@users.noreply.github.com>
2024-02-23 07:00:26 +0000

    mathAndNumber done

---
+++ basics/07num.js
@@ -0,0 +1,25 @@
+// const rollno = 399
+
+// console.log(rollno)
+
+// const rollno2 = new Number(3343)
+
+// console.log(rollno2)
+
+// console.log(typeof(rollno2.toString()))
+
+// console.log(Math)
+
+// console.log(Math.round(12.23))
+
+const number = new Number(234.5323)
+
+// console.log(number.toPrecision(3))
+
+const min = 10
+
+const max = 20
+
+// console.log((Math.floor(Math.random() + 1) + min))
+
+// console.log(number.toFixed(3))
