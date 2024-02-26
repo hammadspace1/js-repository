75ecffb1d12debe668ea5647be9e629693e2774e
Hammad Khan <154624434+hammadspace1@users.noreply.github.com>
2024-02-26 06:51:08 +0000

    array1 done

---
+++ basics2/arrays.js
@@ -0,0 +1,54 @@
+let arr1 = ["hammad", "Khan","tordher"]
+
+// console.log(arr1[1])
+
+
+// let myarr = new Array(2,3,5,6,7)
+
+// console.log(myarr)
+
+// console.log(arr1)
+
+arr1.push("12345")
+
+// console.log(arr1)
+
+arr1.pop()
+
+// console.log(arr1)
+
+let arr2 = arr1.slice(1,3)
+
+// console.log(arr2)
+// console.log(arr1)
+
+let arr3 = arr1.splice(1,3)
+
+// console.log(arr3)
+// console.log(arr1)
+
+arr1.push("khan","tordher","12346")
+
+// console.log(arr1)
+console.log(arr1[3])
+
+arr1.unshift("Male")
+
+console.log(arr1[3])
+
+arr1.shift()
+
+console.log(arr1)
+
+console.log(arr1.includes("tordher"))
+
+let arr4 = arr1.toString()
+
+console.log(typeof arr1)
+
+console.log(typeof arr4)
+
+let arr5 = arr1.join()
+
+console.log(arr5)
+console.log(typeof arr5)
