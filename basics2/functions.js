68df446545b9e5b18bf761e639ae522e49c50c8b
Hammad Khan <154624434+hammadspace1@users.noreply.github.com>
2024-02-27 06:07:23 +0000

    function done

---
+++ basics2/functions.js
@@ -0,0 +1,46 @@
+// function userInfo(userName){
+//     if(!userName){
+//         console.log("Please Enter a Valid UserName")
+//         return 
+//     }
+//     return `${userName} has just logged in`
+// }
+
+// console.log(userInfo("Jamal"))
+
+// function addmultipleno(...numbers){
+//     return numbers
+// }
+
+// let result = addmultipleno(23,34,45,34,23,45,23)
+
+// let v = 0
+// console.log(typeof result)
+
+// for(let i = 0; i<=result.length; i++){
+//     v= v + result[i]
+// }
+// console.log(`Sum is ${v}`)
+
+
+// console.log(result)
+
+// let userinfo ={
+//     name: "hammad",
+//     city: "tordher",
+//     phone: 1233233
+// }
+
+// function foruser(getinfo){
+//     return console.log(`Hi ${getinfo.name} You are From ${getinfo.city} Your Phone Number is ${getinfo.phone}`)
+// }
+
+// foruser(userinfo)
+
+// let array = ["firstPerson","SecondPerson","thirdPerson"]
+
+// function returnSecondName(Names){
+//     return Names[1]
+// }
+
+// console.log(returnSecondName(array))
